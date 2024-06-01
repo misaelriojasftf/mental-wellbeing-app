c782145eb82c4b30bfe9e09711b2d79301c49012
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2024-06-01 15:51:09 +0200

    feat(auth): create login & register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto, LoginUserDto } from '../user/dto/user.dto';
+import { AuthService } from './auth.service';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto, LoginUserDto } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) { }
+
+  async register(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const newUser = await this.userService.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    return newUser;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.userService.findByEmail(loginUserDto.email);
+    if (user && await bcrypt.compare(loginUserDto.password, user.password)) {
+      const payload = { username: user.username, sub: user.id };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    }
+    return null;
+  }
+}
