cd3c5d963eec346470843ff263796e3825cce3dc
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2024-06-01 15:50:24 +0200

    feat(user): create user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService]
 })
-export class UserModule {}
+export class UserModule { }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | undefined> {
+    const user = await this.usersRepository.findOneBy({ id });
+    return user ? plainToInstance(User, user) : undefined;
+  }
+
+  async findCompletedActivities(userId: number): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { id: userId }, relations: ['activities'] });
+  }
+}
