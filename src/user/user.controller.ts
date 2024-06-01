d21619883c37ff2cfa5a153e23e5eea9eb7d2581
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2024-06-01 15:55:40 +0200

    feat(user): controller - get user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { JwtUser } from './user.decorator';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getUser(@JwtUser() user: User) {
+        return this.userService.findById(user.id);
+    }
+}
