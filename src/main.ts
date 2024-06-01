4d78fda2d739536bd32e20ebe3addd37bae8c7f6
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2024-06-01 16:01:34 +0200

    feat(main): add config and validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Mental Wellbeing API')
+    .setDescription('API for managing mental wellbeing activities')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
