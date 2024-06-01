664f86d8b885afac222f920bbe41e3e2b8d8775f
misaelriojasftf <48217522+misaelriojasftf@users.noreply.github.com>
2024-06-01 15:52:51 +0200

    feat(activity): create activity enum

---
+++ src/activity/activity.enum.ts
@@ -0,0 +1,8 @@
+export enum ActivityCategory {
+    RELAXATION = 'Relaxation',
+    SELF_ESTEEM = 'Self-Esteem',
+    PRODUCTIVITY = 'Productivity',
+    PHYSICAL_HEALTH = 'Physical Health',
+    SOCIAL_CONNECTION = 'Social Connection',
+  }
+  
